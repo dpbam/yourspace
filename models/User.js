df3cdc228e24aeee1d5562e0bf082d227b7964c3
dpbam <derekphtpt@gmail.com>
2021-09-05 21:15:54 -0600

    models might be done

--- models/User.js
+++ models/User.js
@@ -1,23 +1,36 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [ThoughtSchema],
+    friends: [UserSchema],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  },
-  thoughts: [ThoughtSchema],
-  friends: [UserSchema],
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 UserSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
+
+const User = model("User", UserSchema);
+
+module.exports = User;
